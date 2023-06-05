deee7fc50ab61e1b98327045bfbc2bc1905b829e
Yash Gurjar <ys.gurjar@gmail.com>
2023-06-05 20:11:13 +0000

    feat: write pseudo code and a logic to draw game area

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 # Inspired from https://towardsdatascience.com/
 # prettify-your-terminal-text-with-termcolor-and-pyfiglet-880de83fda6b
 
+import sys
 import curses
 from termcolor import colored
 from pyfiglet import Figlet
@@ -17,16 +18,32 @@ print(70*'#')
 # Rules
 f = Figlet(font="contessa")
 print(colored(f.renderText("RULES"), 'red'))
-print("""Eat your food and get as big as you can. 
+print("""You are a snake.Eat your prey and get as big as you can.
 Use arrow keys (up, down, left, right) to navigate.
 Do not eat yourself. Do not hit the wall.
 """)
 print(70*'#')
 
+# Build game
+
+wall = []
+
+
+def initialize(stdscr):
+    """Draw initial board, snake and food."""
+    # Draw board. Coordinates start from top left, in the format of y, x.
+    for i in range(20):
+        stdscr.addstr(i,curses.COLS-1,"O")
+        stdscr.addstr(i,0,"O")
+    for i in range(curses.COLS):
+        stdscr.addstr(0,i,"O")
+        stdscr.addstr(20,i,"O")
+    # Draw snake
+
+    # Draw food
+        
 # Creation of window object is taken from https://www.developer.com/languages/python/python-curses-text-drawing/
 
-import curses
-import sys
 
 def main():
   # BEGIN ncurses startup/initialization...
@@ -54,17 +71,45 @@ def main():
   caughtExceptions = ""
   try:
     # Coordinates start from top left, in the format of y, x.
-    stdscr.addstr(0, 0, "Hello, world!")
-    screenDetailText = "This screen is [" + str(curses.LINES) + "] high and [" + str(curses.COLS) + "] across."
-    startingXPos = int ( (curses.COLS - len(screenDetailText))/2 )
-    stdscr.addstr(3, startingXPos, screenDetailText)
-    stdscr.addstr(5, curses.COLS - len("Press a key to quit."), "Press a key to quit.")
+    # stdscr.addstr(0, 0, "Hello, world!")
+    # screenDetailText = "This screen is [" + str(curses.LINES) + "] high and [" + str(curses.COLS) + "] across."
+    # startingXPos = int ( (curses.COLS - len(screenDetailText))/2 )
+    # stdscr.addstr(3, startingXPos, screenDetailText)
+    # stdscr.addstr(5, curses.COLS - len("Press a key to quit."), "Press a key to quit.")
+
+    # # Autheticate user
+    # is_user_autheticated=autheticate()
+
+    # # If user is autheticated, start the game
+    # if is_user_autheticated==True:
+    #   # Draw initial board, snake and food, set game score to zero
+      initialize(stdscr)
+
+    # # Loop
+    #   while True:
+    #   # Move snake
+    #     snake.move(direction)
+
+    #   # Following the move, check the object grabbed i.e. wall, itself or food
+    #     grabbed_object=snake.grabbed_object()
+
+    #   # Depending on the grabbed object, either terminate the game or increase snake size
+    #     if grabbed_object==wall or grabbed_object== itself:
+    #     # Terminate game
+    #     # Compare user's current score against recorded high score, update if necessary
+    #     # Take user back to home page
+    #     elif grabbed_object= food:
+    #     #update snake with new head and body
+    #     #update high score for given user
+    #     snake.update()
+    #     else:
+    #     # continue moving snake in same direction
 
     # Actually draws the text above to the positions specified.
-    stdscr.refresh()
+      stdscr.refresh()
 
     # Grabs a value from the keyboard without Enter having to be pressed (see cbreak above)
-    stdscr.getch()
+      stdscr.getch()
   except Exception as err:
    # Just printing from here will not work, as the program is still set to
    # use ncurses.
