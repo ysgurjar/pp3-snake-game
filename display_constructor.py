efffa6594fb6f28e4e8579c70913013a847ba190
Yash Gurjar <ys.gurjar@gmail.com>
2023-06-06 18:09:11 +0000

    fix: logic to build display and display game boundries on screen

---
+++ display_constructor.py
@@ -0,0 +1,95 @@
+import curses
+
+# BEGIN ncurses startup/initialization...
+# Initialize the curses object. Stdscr stands for standard screen i.e. display in the terminal
+stdscr = curses.initscr()
+
+def display_caught_exceptions(caughtExceptions):
+# Display Errors if any happened:
+    if "" != caughtExceptions:
+        print ("Got error(s) [" + caughtExceptions + "]")
+
+def start_screen(stdscr):
+
+    try:
+        # Do not echo keys back to the client.
+
+        curses.noecho()
+
+        # Non-blocking or cbreak mode... do not wait for Enter key to be pressed.
+        curses.cbreak()
+
+        # Turn off blinking cursor
+        curses.curs_set(False)
+
+        # Enable color if we can...
+        if curses.has_colors():
+            curses.start_color()
+
+        # Actually draws the text above to the positions specified.
+        stdscr.refresh()
+
+        # Grabs a value from the keyboard without Enter having to be pressed (see cbreak above)
+        stdscr.getch()
+
+        # Turn off cbreak mode...
+        curses.nocbreak()
+
+        # Turn echo back on.
+        curses.echo()
+
+        # Restore cursor blinking.
+        curses.curs_set(True)
+
+        # Turn off the keypad...
+        # stdscr.keypad(False)
+
+        # Restore Terminal to original state.
+        curses.endwin()
+    except Exception as err:
+        # Just printing from here will not work, as the program is still set to
+        # use ncurses.
+        print ("Some error [" + str(err) + "] occurred.")
+        caughtExceptions = str(err)
+        display_caught_exceptions(caughtExceptions)
+         
+def end_screen(stdscr):
+     # Turn off cbreak mode...
+        curses.nocbreak()
+
+        # Turn echo back on.
+        curses.echo()
+
+        # Restore cursor blinking.
+        curses.curs_set(True)
+
+        # Turn off the keypad...
+        # stdscr.keypad(False)
+
+        # Restore Terminal to original state.
+        curses.endwin()
+
+def wall_constructor():
+    wall_coordinates=set()
+
+    # Constructing vertical walls. remember y,x representation in addstr
+    for i in range(20):
+            stdscr.addstr(i, curses.COLS-1, "O")
+            stdscr.addstr(i, 0, "O")
+            wall_coordinates.add((i,curses.COLS-1))
+            wall_coordinates.add((i,0))
+    # Constructing horizontal wall, remember y,x representation in addstr
+    for i in range(curses.COLS):
+            stdscr.addstr(0, i, "O")
+            stdscr.addstr(20, i, "O")
+            wall_coordinates.add((0,i))
+            wall_coordinates.add((20,i))
+    stdscr.refresh()
+    return wall_coordinates
+
+def calculate_display_coordinates():
+    grid_points=set()
+    for x in range(20):
+        for y in range(curses.COLS-1):
+            grid_points.add((x,y))
+    return grid_points
--- test2.py
+++ test2.py
@@ -1,18 +1,12 @@
 class Board:
-    """Represents display screen for game"""
-    def __init__(self, grid_points):
-        """Reoresents a list of ALL grid-points in x,y coordinate system on which board elements will be drawn"""
-        self.grid_points=grid_points
     
-    def draw_wall(coordinates):
-        """Draws wall at given coordinates on the board"""
-        pass
-
-    def draw_snake(coordinates):
+    grid_points={}
+       
+    def draw_snake(window,head_coordinates):
         """Draws a snake at given coordinates on the board"""
         pass
 
-    def draw_food(coordinates):
+    def draw_food(window,food_coordinates):
         """Draws a food element at given coordinates on grid"""
 
 class BoardElement:
@@ -24,7 +18,7 @@ class Snake(BoardElement):
     def __init__(self, coordinates):
         BoardElement.__init__(self, coordinates)
     
-    def update_snake(coordinates):
+    def update_snake(window,coordinates):
         """Increases snake length"""
 
 class Food(BoardElement):
@@ -39,9 +33,69 @@ class Wall(BoardElement):
     """Represents wall on board"""
     def __init__(self, coordinates):
         BoardElement.__init__(self, coordinates)
+    
 
+class Game:
+    def __init__(self, player_name, high_score, game_status):
+        self.player_name=player_name
+        self.high_score=high_score
+        self.game_status=game_status
+    
+    def terminate():
+        """End game"""
+
+def initialise(window):
+    """Sets up intial board before starting the game"""
+    board=Board()
+    
+    # Get a list of all display coordinates
+    board.grid_points=d.calculate_display_coordinates()
+    
+    # Construct wall, i.e. boundary of the game
+    wall=Wall(d.wall_constructor())
+    
 
 if __name__=="__main__":
-    snake=Snake([(1,1),(2,2),(3,3)])
-    print(snake)
-    print(snake.coordinates)
+    
+    import display_constructor as d
+    
+    # Get termnimal window object which is to be used as in main logic to draw elements
+    window=d.stdscr
+
+    # Start terminal window (as a drawing board)
+    d.start_screen(window)
+    
+
+#   # Start game
+    game=Game("YG",0,"running")
+    initialise(window)
+
+    d.end_screen(window)
+#     # wall_coordianates,
+#     # snake_coordinates,
+#     # food_coordinates)
+
+#     # # Loop
+#     #   while game.status=="running":
+#             # run_game()
+#     #   
+
+# def run_game():
+#     # Move snake
+#     #     snake.move(direction)
+
+#     #   # Following the move, check the object grabbed i.e. wall, itself or food
+#     #     grabbed_object=snake.grabbed_object()
+
+#     #   # Depending on the grabbed object, either terminate the game or increase snake size
+#     #     if grabbed_object==wall or grabbed_object== itself:
+#     #     # Terminate game
+#     #     # Compare user's current score against recorded high score, update if necessary
+#     #     # Take user back to home page
+#     #     elif grabbed_object= food:
+#     #     #update snake with new head and body
+#     #     #update high score for given user
+#     #     snake.update()
+#     #     else:
+#     #     # continue moving snake in same direction
+
