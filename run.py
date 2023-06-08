b969411618edf31c770438b1c169f16b648031f1
Yash Gurjar <ys.gurjar@gmail.com>
2023-06-08 10:02:23 +0000

    fix:move snake with ASDW keys instead of arrow keys

--- run.py
+++ run.py
@@ -154,6 +154,8 @@ if __name__ == "__main__":
     # Start terminal window (as a drawing board)
     d.start_screen(window)
 
+    # Don't block I/O calls
+    window.nodelay(True)
 
 #   # Start game
     game = Game("YG", 0, "running")
@@ -161,18 +163,29 @@ if __name__ == "__main__":
     board, wall, snake, food = initialise(window)
 
     directions = {
-        curses.KEY_UP: (-1, 0),
-        curses.KEY_DOWN: (1, 0),
-        curses.KEY_LEFT: (0, -1),
-        curses.KEY_RIGHT: (0, 1),
+        # 'w':119, 'W': 087 (Up)
+        119: (-1,0),
+        87: (-1,0),
+        # 'a':97 , 'A': 65 (Left)
+        97: (0,-1),
+        65: (0,-1),
+        # 'd':100 , 'D': 68 (Right)
+        100 : (0, 1),
+        68: (0, 1),
+        # 's' : 114 , 'S': 083 (Down)
+        115:(1, 0),
+        83 : (1,0)
         }
 
-    direction = directions[curses.KEY_RIGHT]
+    direction = directions[100]
     
     for i in range(3):
         # remove previously drawn snake
         snake.remove(window)
-        # move snake to new coordinates
+        # move snake to new coordinates either based
+        # on default behaviour or uesr key input
+        direction = directions.get(window.getch(), direction)
+        
         snake.move_snake(direction)
         # redraw snake
         board.draw_snake(window, snake.coordinates)
