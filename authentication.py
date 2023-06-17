e967a7749ba9d0d31217470e3c294399c1f6fb79
Yash Gurjar <ys.gurjar@gmail.com>
2023-06-17 15:36:07 +0000

    feat:add google spreadsheet credentials

--- authentication.py
+++ authentication.py
@@ -1,3 +1,21 @@
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS=Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS=CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT=gspread.authorize(SCOPED_CREDS)
+SHEET=GSPREAD_CLIENT.open('pp3_user_data')
+user_data_spreadsheet=SHEET.worksheet('user_data')
+data=user_data_spreadsheet.get_all_values()
+usernames=user_data_spreadsheet.col_values(1)[1:]
+print(data)
+print(usernames)
 
 def choose_signing_option():
     selected_option = input(
